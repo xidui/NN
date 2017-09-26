65e227bde07c7df8055bee8df07a86074be45b5d
xidui <664984593@qq.com>
2017-09-26 02:02:12 -0400

    add readme

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# NN
+
+### how to use this code
+```python
+for rate in [0.01, 0.2, 0.5]:
+    for momentum in [0.0, 0.5, 0.9]:
+        nn = NN(seed=2017, learning_rate=rate, dimensions=(784, 100, 10), active_function=sigmoid, momentum=momentum)
+        nn.train(train_data_file='digitstrain.txt', validate_data_file='digitsvalid.txt', epoches=5000, batch=3000)
+```
+Graph will be generated in pictures directory.
+
+### how to plot model
+```python
+from nn import NN
+n = NN()
+n.load_model('model_data_in_models_dir')
+n.plot('model_data_in_models_dir')
+```
--- nn.py
+++ nn.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 import os
 
+eps = 0.01
 
 def sigmoid(X):
     return 1 / (1 + np.exp(-X))
@@ -43,7 +44,11 @@ class NN:
             'W': [],
             'B': [],
             'dW': [],
-            'dB': []
+            'dB': [],
+            'gamma': [],
+            'beta': [],
+            'dgamma': [],
+            'dbeta': [],
         }
 
         for i in range(len(self.dimensions) - 1):
@@ -54,6 +59,10 @@ class NN:
             self.models['B'].append(np.zeros((1, output_dim)))
             self.models['dW'].append(np.zeros((input_dim, output_dim)))
             self.models['dB'].append(np.zeros((1, output_dim)))
+            self.models['gamma'].append(np.ones(output_dim))
+            self.models['beta'].append(np.zeros(output_dim))
+            self.models['dgamma'].append(np.zeros(output_dim))
+            self.models['dbeta'].append(np.zeros(output_dim))
 
         for dir in ['pictures', 'models', 'plot_data']:
             if not os.path.exists(dir):
@@ -152,33 +161,36 @@ class NN:
 
     def forward_helper(self, X):
         intermediate_X = [X]
+        intermediate_BN_X = []
         for i in range(len(self.dimensions) - 1):
             W = self.models['W'][i]
             B = self.models['B'][i]
             X = intermediate_X[-1]
-
-            if self.bn:
-                _mean = X.mean(axis=0, keepdims=True)
-                var_2 = ((X - _mean) ** 2).mean(axis=0, keepdims=True) + 0.01
-                X = (X - _mean) / np.sqrt(var_2)
-
             U = X.dot(W) + B
+
+            if self.bn and i < len(self.dimensions) - 2:
+                intermediate_BN_X.append(U)
+                _mean = U.mean(axis=0, keepdims=True)
+                var_2 = ((U - _mean) ** 2).mean(axis=0, keepdims=True) + eps
+                U = (U - _mean) / np.sqrt(var_2)
+                U = U * self.models['gamma'][i] + self.models['beta'][i]
+
             if i < len(self.dimensions) - 2:
                 intermediate_X.append(self.active_function(U))
             else:
                 # last layer use softmax
                 intermediate_X.append(softmax(U))
-        return intermediate_X
+        return intermediate_X, intermediate_BN_X
 
     def average_loss(self, X, Y):
         size = Y.size
-        output = self.forward_helper(X)[-1]
+        output = self.forward_helper(X)[0][-1]
         data_loss = np.sum(-np.log(output[range(size), Y]))
         data_loss += self.reg_lambda / 2 * sum([np.sum(np.square(W)) for W in self.models['W']])
         return data_loss * (1. / size)
 
     def predict(self, X):
-        output = self.forward_helper(X)[-1]
+        output = self.forward_helper(X)[0][-1]
         return np.argmax(output, axis=1)
 
     def train(self, train_data_file, validate_data_file, epoches, start_from_latest=True, batch=32):
@@ -230,23 +242,44 @@ class NN:
                 Y_train_batch = Y_train[range(batch_start, batch_end)]
                 training_size_batch = Y_train_batch.size
                 # forward
-                intermediate_X = self.forward_helper(X_train_batch)
+                intermediate_X, intermediate_BN_X = self.forward_helper(X_train_batch)
 
                 # backward
                 output = intermediate_X[-1]
                 D = output
                 D[range(training_size_batch), Y_train_batch] -= 1
                 for i in range(len(self.dimensions) - 1)[::-1]:
+                    dbeta, dgamma = None, None
+                    if self.bn and i < len(self.dimensions) - 2:
+                        H = intermediate_BN_X[i]
+                        mu = 1. / training_size_batch * np.sum(H, axis=0)
+                        var = 1. / training_size_batch * np.sum((H - mu) ** 2, axis=0)
+                        dbeta = np.sum(D, axis=0)
+                        dgamma = np.sum((H - mu) * (var + eps) ** (-1. / 2.) * D, axis=0)
+                        D = (1. / training_size_batch) * self.models['gamma'][i] * (var + eps) ** (-1. / 2.) * (
+                            training_size_batch * D - np.sum(D, axis=0) - (H - mu) * (var + eps) ** (-1.0) * np.sum(D * (H - mu), axis=0))
+
                     dW = intermediate_X[i].T.dot(D) / training_size_batch
                     dB = np.sum(D, axis=0, keepdims=True) / training_size_batch
                     # weight decay (L2)
                     dW += self.reg_lambda * self.models['W'][i]
+                    if self.bn and i < len(self.dimensions) - 2:
+                        dbeta  += self.reg_lambda * self.models['beta'][i]
+                        dgamma += self.reg_lambda * self.models['gamma'][i]
                     # momentum
                     dW = -self.learning_rate * dW + self.momentum * self.models['dW'][i]
                     dB = -self.learning_rate * dB + self.momentum * self.models['dB'][i]
+                    if self.bn and i < len(self.dimensions) - 2:
+                        dbeta  = -self.learning_rate * dbeta  + self.momentum * self.models['dbeta'][i]
+                        dgamma = -self.learning_rate * dgamma + self.momentum * self.models['dgamma'][i]
+                        self.models['beta'][i] += dbeta
+                        self.models['gamma'][i] += dgamma
                     self.models['W'][i] += dW
                     self.models['B'][i] += dB
                     # save dW for next iteration
+                    if self.bn and i < len(self.dimensions) - 2:
+                        self.models['dbeta'][i] = dbeta
+                        self.models['dgamma'][i] = dgamma
                     self.models['dW'][i] = dW
                     self.models['dB'][i] = dB
                     # calculate new D
@@ -305,28 +338,33 @@ class NN:
 
 if __name__ == '__main__':
     # problem a, b
-    for seed in range(2017, 2023):
-        nn = NN(seed=seed, learning_rate=0.01, dimensions=(784, 100, 10), momentum=0.0, active_function=sigmoid)
-        nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=2000, batch=32)
+    # for seed in range(2017, 2023):
+    #     nn = NN(seed=seed, learning_rate=0.01, dimensions=(784, 100, 10), momentum=0.0, active_function=sigmoid)
+    #     nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=2000, batch=3000)
     # problem c
 
     # problem d e f
-    for rate in [0.01, 0.2, 0.5]:
-        for momentum in [0.0, 0.2, 0.5, 0.9]:
-            for hidden in [20, 100, 200, 500]:
-                for reg in [0.001, 0.0001, 0.00005, 0.00001]:
-                    nn = NN(seed=2017, learning_rate=rate, dimensions=(784, hidden, 10), active_function=sigmoid, momentum=momentum, reg_lambda=reg)
-                    nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=1000)
+    # for rate in [0.01, 0.2, 0.5]:
+    #     for momentum in [0.0, 0.2, 0.5, 0.9]:
+    #         for hidden in [20, 100, 200, 500]:
+    #             for reg in [0.001, 0.0001, 0.00005, 0.00001]:
+    #                 nn = NN(seed=2017, learning_rate=rate, dimensions=(784, hidden, 10), active_function=sigmoid, momentum=momentum, reg_lambda=reg)
+    #                 nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=1000)
 
     # problem g
-    for lr in [0.1, 0.3, 0.5]:
-        for mom in [0.2, 0.5, 0.9]:
-            for reg in [0.001, 0.0001, 0.00005, 0.00001]:
-                nn = NN(seed=2017, learning_rate=lr, dimensions=(784, 100, 100, 10), active_function=sigmoid, momentum=mom, reg_lambda=reg)
-                nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=2000, batch=32)
+    # for lr in [0.1, 0.5]:
+    #     for mom in [0.5, 0.9]:
+    #         for reg in [0.00005, 0.00001]:
+    #             nn = NN(seed=2017, learning_rate=lr, dimensions=(784, 100, 100, 10), active_function=sigmoid, momentum=mom, reg_lambda=reg)
+    #             nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=2000, batch=32)
     # problem h
+    for lr in [0.1, 0.5]:
+        for mom in [0.3, 0.5]:
+            for reg in [0.001, 0.00005, 0.00001]:
+                nn = NN(seed=2017, learning_rate=lr, dimensions=(784, 100, 10), active_function=sigmoid, momentum=mom, reg_lambda=reg, batch_norm=True)
+                nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=1000, batch=32)
     # problem i
-    # for act in [relu]:
-    #     nn = NN(seed=2017, learning_rate=0.05, dimensions=(784, 100, 10), active_function=relu, momentum=0.9, reg_lambda=0.5)
-    #     nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=3000, start_from_latest=False)
+    # for act in [relu, sigmoid, tanh]:
+    #     nn = NN(seed=2017, learning_rate=0.05, dimensions=(784, 100, 10), active_function=act, momentum=0.5, reg_lambda=0.0001)
+    #     nn.train(train_data_file='digitstrain.txt', validate_data_file='digitstest.txt', epoches=2000, start_from_latest=True)
     pass
